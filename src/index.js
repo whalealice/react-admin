4b451b193464eb65b83fc69c64f8528158de1383
baifan <baifan@hualal.com>
2018-12-07 11:00:02 +0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom';
+export class APP extends Component {
+    render() {
+        return (
+            <div> Hello world!!!!</div >
+        )
+    }
+}
+ReactDOM.render(<APP />, document.getElementById('app')) 
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+var path = require('path');
+var webpack = require('webpack');
+module.exports = {
+    mode: 'development',
+    entry:  path.resolve(__dirname, './src/index.js'),
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: 'bundle.js',
+        // publicPath: '/'
+    },
+    devServer: {
+        historyApiFallback: true,
+        hot: true,
+        inline: true,
+        progress: true,
+        contentBase: path.join(__dirname, "src"), //本地服务器所加载的页面所在的目录
+        port:6060,
+        // proxy:{ //重定向
+        //     "/api": {
+        //         target: "http://localhost:3000",
+        //         pathRewrite: {"^/api" : ""}
+        //     }
+        // }
+    },
+    module: {
+      rules: [
+           {
+                test: /\.(js|jsx)$/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['es2015', 'react'],
+                    }
+                },
+                exclude: /node_modules/
+           }
+        ],
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin()
+    ]
+};
