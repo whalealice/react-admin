1af31f2390525e687287b8304c19a0204f19d14d
baifan <baifan@hualal.com>
2018-12-07 11:41:02 +0800

    first commit

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 export class APP extends Component {
     render() {
         return (
-            <div> Hello world!!!!</div >
+            <div> Hello world!</div >
         )
     }
 }
